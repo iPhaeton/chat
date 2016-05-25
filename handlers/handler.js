09f3540c952d7d9eea9bc5cd22000c1e6621b2ce
Ilya Bohaslauchyk <3751950@gmail.com>
2016-05-25 22:43:20 +0300

    Changed templates to Jade

--- handlers/handler.js
+++ handlers/handler.js
@@ -5,7 +5,7 @@ var errors = require("errors/errors");
 var url = require("url");
 var fs = require("fs");
 var handleError = require("handlers/errorHandler");
-var ejs = require("ejs");
+var jade = require("jade");
 
 function sendPage(file, parameters) {
     var res = parameters.res;
@@ -17,7 +17,7 @@ function sendPage(file, parameters) {
     });
 
     file.on ("end", function () {
-        var renderedData = ejs.render(data, {body: "<h1>Hello, world!</h1>"});
+        var renderedData = jade.render(data, {basedir: "templates"});
 
         res.statusCode = 200;
         res.setHeader("Content-Type", "text/html");
@@ -33,9 +33,33 @@ function sendPage(file, parameters) {
     });
 };
 
+function sendFile(file, parameters) {
+    var res = parameters.res;
+    var data = "";
+
+    file.on ("readable", function () {
+        var nextData = this.read();
+        if (nextData) data += nextData;
+    });
+
+    file.on ("end", function () {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/css");
+        res.end(data);
+    });
+
+    file.on ("error", function (error) {
+        handleError(new errors.RequestError(404, "File not found: " + file.path), parameters);
+    });
+
+    res.on ("close", function () {
+        file.destroy();
+    });
+};
+
 function sendIndex(parameters) {
     var res = parameters.res;
-    var index = new fs.ReadStream("templates/index.ejs");
+    var index = new fs.ReadStream("templates/index.jade");
 
     sendPage(index, parameters);
 };
@@ -46,3 +70,21 @@ function handleForbiddenURL(parameters) {
 
 exports["/"] = sendIndex;
 exports["/forbidden"] = handleForbiddenURL;
+
+//----------------------------------------------------------------------------------------------------
+function sendCSS(parameters) {
+    var res = parameters.res;
+    var index = new fs.ReadStream("templates/layouts/styles.css");
+
+    sendFile(index, parameters);
+};
+
+function sendJS(parameters) {
+    var res = parameters.res;
+    var index = new fs.ReadStream("public/vendor/s.js");
+
+    sendFile(index, parameters);
+};
+
+exports["/templates/layouts/styles.css"] = sendCSS;
+exports["/public/vendor/s.js"] = sendJS;
