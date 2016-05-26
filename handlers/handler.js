c9e17bb8dc613967656559c5fcc5c6d480838893
Ilya Bohaslauchyk <3751950@gmail.com>
2016-05-26 09:55:52 +0300

    Corrected using of templates
    1. All paths (except for "/") are processed by one function
    2. Added an object with allowed paths

--- handlers/handler.js
+++ handlers/handler.js
@@ -7,9 +7,12 @@ var fs = require("fs");
 var handleError = require("handlers/errorHandler");
 var jade = require("jade");
 
-function sendPage(file, parameters) {
+function sendFile(path, parameters) {
+    var file = new fs.ReadStream(path);
     var res = parameters.res;
     var data = "";
+    var ext = getExtention(path);
+    var type = getType(ext);
 
     file.on ("readable", function () {
         var nextData = this.read();
@@ -17,34 +20,10 @@ function sendPage(file, parameters) {
     });
 
     file.on ("end", function () {
-        var renderedData = jade.render(data, {basedir: "templates"});
+        if (ext === "jade") data = jade.render(data, {basedir: "templates"});
 
         res.statusCode = 200;
-        res.setHeader("Content-Type", "text/html");
-        res.end(renderedData);
-    });
-
-    file.on ("error", function (error) {
-        handleError(new errors.RequestError(404, "File not found: " + file.path), parameters);
-    });
-
-    res.on ("close", function () {
-        file.destroy();
-    });
-};
-
-function sendFile(file, parameters) {
-    var res = parameters.res;
-    var data = "";
-
-    file.on ("readable", function () {
-        var nextData = this.read();
-        if (nextData) data += nextData;
-    });
-
-    file.on ("end", function () {
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "text/css");
+        res.setHeader("Content-Type", type);
         res.end(data);
     });
 
@@ -57,11 +36,24 @@ function sendFile(file, parameters) {
     });
 };
 
-function sendIndex(parameters) {
-    var res = parameters.res;
-    var index = new fs.ReadStream("templates/index.jade");
+function getExtention (path) {
+    var arr = path.split(".");
+    return arr[arr.length - 1];
+};
 
-    sendPage(index, parameters);
+function getType (ext) {
+    switch (ext) {
+        case "jade":
+            return "text/html";
+        case "css":
+            return "text/css";
+        case "js":
+            return "text/javascript";
+    };
+};
+
+function sendIndex(parameters) {
+    sendFile("templates/index.jade", parameters);
 };
 
 function handleForbiddenURL(parameters) {
@@ -71,20 +63,4 @@ function handleForbiddenURL(parameters) {
 exports["/"] = sendIndex;
 exports["/forbidden"] = handleForbiddenURL;
 
-//----------------------------------------------------------------------------------------------------
-function sendCSS(parameters) {
-    var res = parameters.res;
-    var index = new fs.ReadStream("templates/layouts/styles.css");
-
-    sendFile(index, parameters);
-};
-
-function sendJS(parameters) {
-    var res = parameters.res;
-    var index = new fs.ReadStream("public/vendor/s.js");
-
-    sendFile(index, parameters);
-};
-
-exports["/templates/layouts/styles.css"] = sendCSS;
-exports["/public/vendor/s.js"] = sendJS;
+exports.file = sendFile;
--- server/router.js
+++ server/router.js
@@ -5,6 +5,12 @@ var handle = require("handlers/handler");
 var handleError = require("handlers/errorHandler")
 var url = require("url");
 var log = require("lib/log")(module);
+var pathMod = require("path");
+
+var paths = {
+    "/public": true,
+    "/templates": true
+};
 
 module.exports = function (req, res) {
     var reqParsed = url.parse(req.url);
@@ -23,8 +29,34 @@ module.exports = function (req, res) {
             handleError(error, parameters);
         };
     }
+    else if (pathname = checkPath(pathname)) {
+        var parameters = {
+            req: req,
+            res: res
+        };
+        handle.file(pathname, parameters);
+    }
     else {
         res.statusCode = 400;
         res.end ("Bad request");
     };
+};
+
+function checkPath (pathname) {
+    try {
+        pathname = decodeURIComponent(pathname);
+    }
+    catch (e) {
+        return false;
+    };
+
+    if (~pathname.indexOf("\0")) return false;
+
+    pathname = pathMod.normalize(pathMod.join(__dirname + "/..", pathname));
+
+    for (var path in paths) {
+        if (pathname.indexOf(pathMod.join(__dirname + "/..", path)) === 0) return pathname;
+    }
+
+    return false;
 };
